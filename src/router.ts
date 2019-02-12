990a55457da6bcdad87198b15094b0099dd88b4a
Meghana <mvadlapa@buffalo.edu>
2019-02-12 00:54:56 -0500

    added navigation links

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Login from '@/views/Login.vue';
+import Register from '@/views/Register.vue';
+import UploadProduct from '@/views/UploadProduct.vue';
 
 Vue.use(Router);
 
@@ -12,6 +15,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+    {
+      path: '/upload-product',
+      name: 'Upload Product',
+      component: UploadProduct,
     },
     {
       path: '/about',
